9671e26a625a4b81c40378ecc4c73662541e8363
Tom MacDougall <macdougt71@gmail.com>
2015-10-04 11:04:09 -0400

    Operating in a temporary directory

--- install_t.bash
+++ install_t.bash
@@ -1,4 +1,19 @@
 # Installer to standard terminal utilities and functionality
+
+# Operate in a temporary directory
+# Check for mktemp
+if ! which mktemp
+then
+   echo "You need to install mktemp to use this script"
+   exit
+fi
+
+
+mytmpdir=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
+echo "This script will be operating in $mytmpdir"
+
+cd $mytmpdir
+
 if [ "$(uname)" == "Darwin" ]; then
    ARCH='mac'
    echo "This terminal appears to be running MAC os"
@@ -28,6 +43,13 @@ fi
 # Grab the files
 wget https://raw.githubusercontent.com/macdougt/bash-examples/master/.bash_aliases -O .bash_aliases
 wget https://raw.githubusercontent.com/macdougt/bash-examples/master/.bashrc -O .bashrc
+wget https://raw.githubusercontent.com/macdougt/bash-examples/master/utils/appendif -O appendif
+wget https://raw.githubusercontent.com/macdougt/bash-examples/master/utils/cdn -O cdn
+wget https://raw.githubusercontent.com/macdougt/bash-examples/master/utils/getDir -O getDir
+wget https://raw.githubusercontent.com/macdougt/bash-examples/master/utils/cleandh -O cleandh
+wget https://raw.githubusercontent.com/macdougt/bash-examples/master/utils/list -O list
+
+chmod +x appendif cdn getDir cleandh list
 
 # Move the files if necessary
 if [[ "$PWD" != "$HOME" ]]
@@ -35,3 +57,9 @@ then
    mv .bashrc $HOME 
    mv .bash_aliases $HOME
 fi
+
+mv appendif cdn getDir cleandh list /usr/local/bin
+
+# Get rid of the temporary directory
+rmdir $mytmpdir
+
