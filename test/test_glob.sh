ade6578a9e1314a6e35d5076c1bd974717952e5c
Tom MacDougall <macdougt71@gmail.com>
2017-12-19 08:41:51 -0500

    Adding glob demo

---
+++ test/test_glob.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+
+# Test with enough args, not validated
+# e.g. test_glob.sh * (in a directory with more than 2 files)
+
+echo "\$1 => $1";
+
+echo "\$2 => $2";
+
+echo "\$* => $*";
+
+
