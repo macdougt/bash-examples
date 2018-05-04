72eb802fcc606aab2ff1150f4ea26e381d389bf0
Tom MacDougall <macdougt71@gmail.com>
2018-05-04 09:02:06 -0400

    Multi-dimensional hash

--- test/test_hash.sh
+++ test/test_hash.sh
@@ -2,3 +2,15 @@
 declare -A MYMAP     # Create an associative array
 MYMAP[foo]=bar       # Put a value into an associative array
 echo ${MYMAP[foo]}
+
+MYMAP[foo2]=bar2       # Put a value into an associative array
+echo ${MYMAP[foo2]}
+declare -A MYMAP2[foo]     # Create an associative array
+
+MYMAP2[foo,er]=next_level # No real multi-dimensions
+echo ${MYMAP2[foo,er]}
+
+
+
+
+
